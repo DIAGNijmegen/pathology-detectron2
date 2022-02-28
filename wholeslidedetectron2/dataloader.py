b4d9a87ecc8f1a0ea9831791eafe72bea18f2e7c
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-02-28 10:16:23 +0100

    format

--- wholeslidedetectron2/dataloader.py
+++ wholeslidedetectron2/dataloader.py
@@ -6,6 +6,7 @@ from wholeslidedata.iterators import BatchIterator
 
 class WholeSlideDetectron2DataLoader(BatchIterator):
     MIN_DETECTION_SIZE = 4
+
     def __next__(self):
         x_batch, y_batch, _ = super().__next__()
         batch_dicts = []
@@ -20,19 +21,19 @@ class WholeSlideDetectron2DataLoader(BatchIterator):
 
     def _get_gt_boxes(self, y_sample, image_size):
         y_boxes = y_sample[~np.all(y_sample == 0, axis=-1)]
-        boxes = y_boxes[..., :4].astype('int32')
+        boxes = y_boxes[..., :4].astype("int32")
         target_boxes = []
         target_classes = []
         for idx, box in enumerate(boxes):
-            
-            if (box[2]-box[0]) < WholeSlideDetectron2DataLoader.MIN_DETECTION_SIZE:
+
+            if (box[2] - box[0]) < WholeSlideDetectron2DataLoader.MIN_DETECTION_SIZE:
                 continue
-            if (box[3]-box[1]) < WholeSlideDetectron2DataLoader.MIN_DETECTION_SIZE:
+            if (box[3] - box[1]) < WholeSlideDetectron2DataLoader.MIN_DETECTION_SIZE:
                 continue
-            
+
             target_boxes.append(box)
-            target_classes.append(y_boxes[idx][-2]-1)
-            
+            target_classes.append(y_boxes[idx][-2] - 1)
+
         target = Instances(image_size)
         target_boxes = np.array(target_boxes)
         target.gt_boxes = Boxes(target_boxes)
